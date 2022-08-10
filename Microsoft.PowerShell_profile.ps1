5bd01eab214efd22e554cb5dcdd22868fba0b6a2
aaxyat <mail@ayushb.com>
2022-08-10 19:54:38 +0545

    add acm Command and auto install starship

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,14 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
 Invoke-Expression (&starship init powershell)
+
+function acm {
+    param(
+      [Parameter(ValueFromRemainingArguments = $true)]
+      [String[]] $message
+    )
+    git add .
+    git commit -a -m "$message"
+    git push
+  }
+  
