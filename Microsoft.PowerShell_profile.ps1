0e028d9d8a8315eccbc33b112d3b9acf490a8f4c
Ayush Bhattarai <mail@ayushb.com>
2023-01-02 14:57:18 +0545

    remove unnessery variables

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -86,10 +86,6 @@ function Edit-Profile
     }
 }
 
-# We don't need these any more; they were just temporary variables to get to $isAdmin. 
-# Delete them to prevent cluttering up the user profile. 
-Remove-Variable identity
-Remove-Variable principal
 #
 # Aliases
 #
